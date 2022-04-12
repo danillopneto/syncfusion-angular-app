501e7b9bdb29064a44c2db57c704a8cfc7c47672
Danillo Pinheiro Neto <danillopneto@outlook.com>
2022-04-12 18:57:53 -0300

    Simple syncfusion app demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Imported syncfusion Grid module from grids package
+import { GridModule } from '@syncfusion/ej2-angular-grids';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
